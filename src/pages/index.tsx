fc94e87736a1f45c9063b69418fafb1d376cb0e2
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-07-08 12:59:14 +0530

    adding meta data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 import Home from "./Homepage";
+import NextHead from "next/head";
 function index() {
-  return <Home />;
+  return (
+    <>
+      <NextHead>
+        <meta charSet="UTF-8" />
+        <title>TACO</title>
+        <meta name="description" content={"description" || ""} />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+          key="viewport"
+        />
+      </NextHead>
+      <Home />
+    </>
+  );
 }
 
 export default index;
