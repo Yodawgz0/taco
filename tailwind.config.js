1d9849a144f8bcebf73812b8fab6b4c0b2e09646
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-07-08 14:10:06 +0530

    config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        hypBlack: "#000000",
+        hypBlackLight: "#0d0d0d",
+        hypLightPurple: "#9e32ea",
+        hypMedPurple: "#7f17d8",
+        hypDarkPurple: "#410194",
       },
     },
   },
   plugins: [],
-}
+};
